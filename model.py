02724e6f199e4a58d2ac256772da300f74389243
Ammr007 <amarmondair87@gmail.com>
2025-08-26 21:52:52 -0700

    Add files via upload
    Signed-off-by: Ammr007 <amarmondair87@gmail.com>

--- model.py
+++ model.py
@@ -110,9 +110,53 @@ class ResidualConnection(nn.Module):
     def forward(self, x, sublayer):
         return x + self.dropout(sublayer(self.norm(x)))
 
+class EncoderBlock(nn.Module):
+    def __init__(self, attention_block : MultiHeadAttentionBlock, feed_forward : FeedForward, dropout : float):
+        super().__init__()
+        self.attention_block = attention_block
+        self.feed_forward = feed_forward
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
 
+    def forward(self, x, src_mask):
+        x = self.residual_connections[0](x, lambda x: self.attention_block(x, x, x, src_mask))
+        x = self.residual_connections[1](x, self.feed_forward)
+        return x
 
+class Encoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList):
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
 
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+
+class DecoderBlock(nn.Module):
+    def __init__(self, attention_block : MultiHeadAttentionBlock, feed_forward : FeedForward, dropout : float, cross_attention_block):
+        super().__init__()
+        self.attention_block = attention_block
+        self.feed_forward = feed_forward
+        self.cross_attention_block = cross_attention_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        x = self.residual_connections[0](x, lambda x: self.attention_block(x, x, x, tgt_mask))
+        x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+        x = self.residual_connections[2](x, self.feed_forward)
+        return x
+
+class Decoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList):
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, tgt_mask)
+        return self.norm(x)
 
 
 
